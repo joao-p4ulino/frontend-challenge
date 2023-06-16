1da297c7154a2f8da1f4e0c41ddb7a050a424af3
João Paulino <joao.santos@ixcsoft.com.br>
2023-06-16 15:07:26 -0300

    feat: creating header

---
+++ capputeeno/src/components/header.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import styled from "styled-components";
+
+import {Saira_Stencil_One} from 'next/font/google'
+import {PrimaryInput, PrimaryInputWSearchIcon} from "@/components/primary-input";
+import {CartControl} from "@/components/cart-control";
+
+const sairaStencil = Saira_Stencil_One({
+    weight: ['400'],
+    subsets: ['latin']
+})
+
+interface HeaderProps {
+
+}
+
+const TagHeader = styled.header`
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+  padding: 20px 160px;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+  }
+`
+
+const Logo = styled.a`
+  color: var(--logo-color);
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 150%;
+`
+
+export function Header(props: HeaderProps) {
+    return (
+        <TagHeader>
+            <Logo className={sairaStencil.className}>capputeeno</Logo>
+            <div>
+                <PrimaryInputWSearchIcon placeholder="Procurando por algo específico?"/>
+                <CartControl/>
+            </div>
+        </TagHeader>
+    );
+}
