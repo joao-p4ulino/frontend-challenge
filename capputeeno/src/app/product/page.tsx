49fbdcd57e821801254ed06e2fb532c707bcd988
João Paulino <joaopaulopaulinodossantos@gmail.com>
2023-06-26 17:33:28 -0300

    feat: add btn add to cart control

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -57,57 +57,78 @@ const Container = styled.div`
 `
 
 const ProductInfo = styled.div`
-    display: flex;
-    align-items: flex-start;
-    justify-content: center;
-    flex-direction: column;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
 
-    span {
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 150%;
-        color: var(--text-dark-2);
-    }
+  span {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    color: var(--text-dark-2);
+  }
 
-    h2 {
-        font-weight: 300;
-        font-size: 32px;
-        line-height: 150%;
-        color: var(--text-dark-2);
-        margin-top: 12px;
-    }
+  h2 {
+    font-weight: 300;
+    font-size: 32px;
+    line-height: 150%;
+    color: var(--text-dark-2);
+    margin-top: 12px;
+  }
 
-    span:nth-of-type(2){
-        font-weight: 600;
-        font-size: 20px;
-        color: var(--shapes-dark);
-        margin-bottom: 24px;
+  span:nth-of-type(2) {
+    font-weight: 600;
+    font-size: 20px;
+    color: var(--shapes-dark);
+    margin-bottom: 24px;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 12px;
+    color: (--text-dark);
+    margin-bottom: 24px;
+  }
+
+  div {
+    h3 {
+      text-transform: uppercase;
+      color: var(--text-dark);
+      font-weight: 500;
+      font-size: 16px;
     }
 
     p {
-        font-weight: 400;
-        font-size: 12px;
-        color: (--text-dark);
-        margin-bottom: 24px;
+      font-size: 14px;
     }
-
-    div {
-        h3 {
-            text-transform: uppercase;
-            color: var(--text-dark);
-            font-weight: 500;
-            font-size: 16px;
-        }
-
-        p {
-            font-size: 14px;
-        }
-    }
+  }
 `
 
-export default function Product({ searchParams }: { searchParams: { id: string }}) {
-    const { data } = useProduct(searchParams.id);
-    return(
+export default function Product({searchParams}: { searchParams: { id: string } }) {
+    const {data} = useProduct(searchParams.id);
+
+    const handleAddToCard = () => {
+        let cartItems = localStorage.getItem('cart-items');
+        if (cartItems) {
+            let cartItemsArray = JSON.parse(cartItems);
+
+            let existingProductIndex = cartItemsArray.findIndex((item: { id: string; }) => item.id === searchParams.id);
+
+            if (existingProductIndex != -1) {
+                cartItemsArray[existingProductIndex].quantity += 1;
+            } else {
+                cartItemsArray.push({...data, quantity: 1, id: searchParams.id})
+            }
+
+            localStorage.setItem('cart-items', JSON.stringify(cartItemsArray));
+        } else {
+            const newCart = [{...data, quantity: 1, id: searchParams.id}]
+            localStorage.setItem('cart-items', JSON.stringify(newCart));
+        }
+    }
+
+    return (
         <DefaultPageLayout>
             <Container>
                 <BackBtn navigate="/"/>
@@ -115,14 +136,14 @@ export default function Product({ searchParams }: { searchParams: { id: string }
                     <img src={data?.image_url}/>
                     <div>
                         <ProductInfo>
-                                <span>{data?.category}</span>
-                                <h2>{data?.name}</h2>
-                                <span>{formatPrice(data?.price_in_cents ?? 0)}</span>
-                                <p>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
-                                <h3>Descrição</h3>
-                                <p>{data?.description}</p>
+                            <span>{data?.category}</span>
+                            <h2>{data?.name}</h2>
+                            <span>{formatPrice(data?.price_in_cents ?? 0)}</span>
+                            <p>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
+                            <h3>Descrição</h3>
+                            <p>{data?.description}</p>
                         </ProductInfo>
-                        <button>
+                        <button onClick={handleAddToCard}>
                             <ShopBagIcon/>
                             Adicionar ao carrinho
                         </button>
