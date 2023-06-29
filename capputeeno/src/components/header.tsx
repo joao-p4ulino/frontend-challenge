290be0eb6780bf1c004410134a4adf7a997f30f7
João Paulino <joaopaulopaulinodossantos@gmail.com>
2023-06-29 10:51:09 -0300

    feat: add navigation to home

--- capputeeno/src/components/header.tsx
+++ capputeeno/src/components/header.tsx
@@ -39,6 +39,7 @@ const Logo = styled.a`
   font-weight: 400;
   font-size: 20px;
   line-height: 150%;
+  text-decoration: none;
   
   @media (min-width: ${props => props.theme.tableBreakpoint}) {
     font-size: 24px;
@@ -53,7 +54,7 @@ export function Header(props: HeaderProps) {
     const {setSearch, search} = useFilter();
     return (
         <TagHeader>
-            <Logo className={sairaStencil.className}>Capputeeno</Logo>
+            <Logo className={sairaStencil.className} href="/">Capputeeno</Logo>
             <div>
                 <PrimaryInputWSearchIcon
                     value={search}
